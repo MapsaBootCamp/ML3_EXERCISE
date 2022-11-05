c1d206a0f04c33e9dc055261f704a70ab8768b29
ayatfoad <113610049+ayatfoad@users.noreply.github.com>
2022-11-05 12:16:07 -0700

    1539

---
+++ 1539b.py
@@ -0,0 +1,12 @@
+l,r=1,5
+# 2 5
+# 1 7
+result=0
+alphbet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
+'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+que = str[(l-1):(r)]
+for i in que:
+     result+=alphbet.index(i)+1
+print(result)
