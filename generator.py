6b792fb302f8e51a51a36a32c614303da41caa0c
mmz1380 <mohammad.mesbah.mz.1380@gmail.com>
2022-12-07 15:23:37 +0330

    W4 generator.py

--- generator.py
+++ generator.py
@@ -1,2 +1,44 @@
-def hundred_char():
-    pass
+def hundred_char(filename: str, size=100):
+    try:
+        with open(f"{filename}.txt", "r+") as f:
+            f.seek(0)
+            while True:
+                if len(chars := f.read(size)):
+                    yield chars
+                else:
+                    break
+    except:
+        print("false")
+
+
+f = open("testies.txt", "w+")
+f.write("""kjhajdhajshjaksfhkjhajdhajshjaksfhkjhajdhajshjaksfhfhjh;khkjhajdhajshjaksfhkjhajdhajshjaksfh
+kjdhvsjdhvkjhajdhajshjaksfhkjhajdhajshjaksfhkjhajdhajshjaksfhjh;khkjhajdhajshjaksfhkjhaadwueyqouie
+acscsjv vJodj HFkdhgjk;hbvljcnbdlsfg4qf 21a5f4 3sdfnsdjh;khkjhajdhajshjaksfhkjhajdhajshjaksfhsadasd
+kjhajdhajshjaksfhkjhajdhajshjaksfhkjhajdhajshjaksfhfhjh;khkjhajdhajshjaksfhkjhajdhajshjaksfh
+kjdhvsjdhvkjhajdhajshjaksfhkjhajdhajshjaksfhkjhajdhajshjaksfhjh;khkjhajdhajshjaksfhkjhaadwueyqouie
+acscsjv vJodj HFkdhgjk;hbvljcnbdlsfg4qf 21a5f4 3sdffhjh;khksfhjh;khkjhajdhajshjaksfhkjhaadwueyqouie
+acscsjv vJodj HFkdhgjk;hbvljcnbdlsfg4qf 21a5f4 3sdfnsdkjhajdhajshjaksfhkjhajdhajshjaksfh
+kjdhvsjdhvkjhajdhajshjaksfhkjhajdhajshjaksfhkjhajdhajsksfhjh;khkjhajdhajshjaksfhkjhaadwueyqouie
+acscsjv vJodj HFkdhgjk;hbvljcnbdlsfg4qf 21a5f4 3sdfnsdhjaksfhjh;khkjhajdhajshjaksfhkjhaadwueyqouie
+acscsjv vJodj HFkdhgjk;hbvljcnbdlsfg4qf 21a5f4 3sdfnsdvksfhjh;khkjhajdhajshjaksfhkjhaadwueyqouie
+acscsjv vJodj HFkdhgjk;hbvljcnbdlsfg4qf 21a5f4 3sdfnsdnsdjh;khkjhajdhajshjaksfhkjhajdhajshjaksfhsdvdsvoja
+asdjkkjhajdhajshjaksfhkjhajdhajshjaksfhkjhajdhajshjaksfhjhfhjh;khkjhajdhajshjaksfhkjhajdhajshjaksfh
+kjdhvsjdhvkjhajdhajshjaksfhkjhajdhajshjaksfhkjhajdhajshjakvsfhjh;khkjhajdhajshjaksfhkjhaadwueyqouie
+acscsjv vJodj HFkdhgjk;hbvljcnbdlsfg4qf 21a5f4 3sdfnsd;khkjhajdhajshjaksfhkjhajdhajshjaksfh
+kjdhvsjdhvkjhajdhajshjaksfhkjhajdhajshjaksfhkjhajdhfhjh;khkvjhajdhajshjaksfhkjhajdhajshjaksfh
+kjdhvsjdhvkjhajdhajshjaksfhkjhajdhajshjaksfhkjhajaksfhsdvdsvoja
+asdjkkjhajdhajshjaksfhkjhajdhajshjaksfhkjhajdhajshjaksfhjhfhjh;khkjhajdhajshjaksfhkjhajdhajshjaksfh
+kjdhvsjdhvkjhajdhajshjaksfhkjhajdhajshjaksfhkjhajdhajshjakvsfhjh;khkjhajdhajshjaksfhkjhaadwueyqouie
+acscsjv vJodj HFkdhgjk;hbvljcnbdlsfg4qf 21a5f4 3sdfnsd;khkjhajdhajshjaksfhkjhajdhajshjaksfh
+kjdhvsjdhvkjhajaksdhasd
+sadasdsa dsad asdwo29138
+asdasdasjkdjjlaskdjowajdhajshjaksfhkjhajdhajshjaksfhkjhajdhfhjh;khkvjhajdhajshjaksfhkjhajdhajshjaksfh
+kjdhvsjdhvkjhajdhajshjaksfhkjhajdhajshjaksfhkjhajdhajdksfhjh;khkjhajdhajshjaksfhkjhaadwueyqouie
+acscsjv vJodj HFkdhgjk;hbvljcnbdlsfg4qf 21a5f4 3sdfnsdh;khkvjhajdhajshjaksfhkjhajdhajshjaksfh
+kjdhvsjdhvkjhajdhajshjaksfhkjhajdhajshjaksfhkjhajdhajdksfhjh;khkjhajdhajshjaksfhkjhaadwueyqouie
+acscsjv vJodj HFkdhgjk;hbvljcnbd adf4 3sdfnssadkhdsaQWEkjhajdhajshjaksfhkjhaadwueyqouie
+acscsjv vJodj HFkdhgjk;hbvljcnbdlsfg4qf 21a5f4 3sdfnsd""")
+f.close()
+for i, j in enumerate(hundred_char("testies")):
+    print(i, j, "\n")
